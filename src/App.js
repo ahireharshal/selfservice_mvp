516054a417846ac748af9ef8ea104e1e7a902901
ahireharshal <ahireharshal@gmail.com>
2020-10-20 13:02:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React,{useContext} from 'react';
-import Typography from '@material-ui/core/Typography';
 import './App.css'
 
-
 import Chat from '../src/components/Chat/Chat'
-
-
-import Report from './components/Report/Report'
-import Dashboard from './components/Dashboard/Dashboard';
 import ConfirmUser from '../src/components/ConfirmUser/ConfirmUser'
 
 import { MusicPlayerProvider } from "./appContext";
